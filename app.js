7ba56697713dcbef90559b52275605666a810154
Elvy1999 <elvydecoo99@gmail.com>
2023-10-04 19:28:39 -0400

    Fixed a bug with the containers overlaping after the main container had been cleared and a new item was added

--- app.js
+++ app.js
@@ -83,24 +83,25 @@ function addTodo(e) {
 //toggles emptylist display depending on whether todolist is empty or not
 function toggleEmptyDisplay() {
   if (allTodoItems.childElementCount - 1 == 0 && emptyList.classList.contains("showDisplay")) {
-    console.log(allTodoItems.childElementCount);
     emptyList.classList.toggle("showDisplay");
-    allTodoItems.style.display = "none";
+    allTodoItems.classList.toggle("showDisplay");
   } else if (!emptyList.classList.contains("showDisplay") && allTodoItems.childElementCount == 1) {
-    console.log("hello bitch");
     emptyList.classList.toggle("showDisplay");
-    allTodoItems.style.display = "inline";
+    allTodoItems.classList.toggle("showDisplay");
   }
 }
 
 //switch todolist containers
 function switchContainers(e) {
-  const allContainers = document.querySelectorAll(".items");
-  allContainers.forEach((container) => {
-    container.classList.add("showDisplay");
-  });
-  const container = document.querySelector(`.${e.target.id}`);
-  container.classList.toggle("showDisplay");
+  if (emptyList.classList.contains("showDisplay")) {
+    console.log("In the condition");
+    const allContainers = document.querySelectorAll(".items");
+    allContainers.forEach((container) => {
+      container.classList.add("showDisplay");
+    });
+    const container = document.querySelector(`.${e.target.id}`);
+    container.classList.toggle("showDisplay");
+  }
 }
 
 //Event Listeners
